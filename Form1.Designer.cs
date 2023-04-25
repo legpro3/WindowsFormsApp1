f970e85f71038338b2768c820a0196c8b40419fb
apgg1 <apgg1@DESKTOP-KV10TKM>
2023-04-24 22:52:11 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCEP = new System.Windows.Forms.TextBox();
+            this.HttpClient = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtCEP
+            // 
+            this.txtCEP.Location = new System.Drawing.Point(280, 115);
+            this.txtCEP.Name = "txtCEP";
+            this.txtCEP.Size = new System.Drawing.Size(100, 20);
+            this.txtCEP.TabIndex = 0;
+            // 
+            // HttpClient
+            // 
+            this.HttpClient.Location = new System.Drawing.Point(209, 149);
+            this.HttpClient.Name = "HttpClient";
+            this.HttpClient.Size = new System.Drawing.Size(110, 34);
+            this.HttpClient.TabIndex = 2;
+            this.HttpClient.Text = "Pesquisar";
+            this.HttpClient.UseVisualStyleBackColor = true;
+            this.HttpClient.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(206, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Insira o CEP:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(896, 480);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.HttpClient);
+            this.Controls.Add(this.txtCEP);
+            this.Name = "Form1";
+            this.Text = "Pesquisador de CEP";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCEP;
+        private System.Windows.Forms.Button HttpClient;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Npgsql;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            bool enderecoEncontrado = true;
+            while (enderecoEncontrado)
+            {
+                string cep = txtCEP.Text;
+                using (var httpClient = new HttpClient())
+                {
+                    var response = httpClient.GetAsync($"https://viacep.com.br/ws/{cep}/json/").Result;
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var content = response.Content.ReadAsStringAsync().Result;
+                        var endereco = JsonConvert.DeserializeObject<Endereco>(content);
+                        if (endereco != null && !string.IsNullOrEmpty(endereco.CEP))
+                        {
+                            MessageBox.Show("Endereço encontrado." +
+                                $"\nCEP: {cep}" +
+                                $"\nLogradouro: {endereco.Logradouro}" +
+                                $"\nComplemento: {endereco.Complemento}" +
+                                $"\nBairro: {endereco.Bairro}" +
+                                $"\nLocalidade: {endereco.Localidade}" +
+                                $"\nUF: {endereco.UF}" +
+                                $"\nIBGE: {endereco.IBGE}" +
+                                $"\nGIA: {endereco.GIA}" +
+                                $"\nDDD: {endereco.DDD}" +
+                                $"\nSIAFI: {endereco.SIAFI}");
+
+                            bool registroExistente = VerificarRegistroExistenteNoBancoDeDados(endereco.CEP);
+                            if (registroExistente)
+                            {
+                                AtualizarEnderecoNoBancoDeDados(endereco);
+                                MessageBox.Show("Endereço atualizado no banco de dados.");
+                            }
+                            else
+                            {
+                                InserirEnderecoNoBancoDeDados(endereco);
+                                MessageBox.Show("Endereço inserido no banco de dados.");
+                            }
+                            DialogResult result = MessageBox.Show("Deseja excluir o endereço?", "Exclusão de Endereço", MessageBoxButtons.YesNo);
+                            if (result == DialogResult.Yes)
+                            {
+                                RemoverEnderecoDoBancoDeDados(endereco.CEP);
+                                MessageBox.Show("Endereço excluído do banco de dados.");
+                            }
+                            enderecoEncontrado = false;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Endereço não encontrado. Por favor, insira o CEP novamente.");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Erro. Por favor, insira o CEP novamente.");
+                    }
+                }
+            }
+        }
+
+        private void InserirEnderecoNoBancoDeDados(Endereco endereco)
+        {
+            {
+                using (var connection = new NpgsqlConnection("Host=localhost:5432;Username=postgres;Password=1478951;Database=postgres"))
+                {
+                    connection.Open();
+
+                    using (var command = new NpgsqlCommand("INSERT INTO endereco (cep, logradouro, complemento, bairro, localidade, uf, ibge, gia, ddd, siafi) " +
+                                                           "VALUES (@cep, @logradouro, @complemento, @bairro, @localidade, @uf, @ibge, @gia, @ddd, @siafi)", connection))
+                    {
+                        command.Parameters.AddWithValue("@cep", endereco?.CEP);
+                        command.Parameters.AddWithValue("@logradouro", endereco?.Logradouro);
+                        command.Parameters.AddWithValue("@complemento", endereco?.Complemento);
+                        command.Parameters.AddWithValue("@bairro", endereco?.Bairro);
+                        command.Parameters.AddWithValue("@localidade", endereco?.Localidade);
+                        command.Parameters.AddWithValue("@uf", endereco?.UF);
+                        command.Parameters.AddWithValue("@ibge", endereco?.IBGE);
+                        command.Parameters.AddWithValue("@gia", endereco?.GIA);
+                        command.Parameters.AddWithValue("@ddd", endereco?.DDD);
+                        command.Parameters.AddWithValue("@siafi", endereco?.SIAFI);
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+
+        private void AtualizarEnderecoNoBancoDeDados(Endereco endereco)
+        {
+            using (var connection = new NpgsqlConnection("Host=localhost:5432;Username=postgres;Password=1478951;Database=postgres"))
+            {
+                connection.Open();
+
+                using (var command = new NpgsqlCommand("UPDATE endereco SET logradouro=@logradouro, complemento=@complemento, " +
+                                                       "bairro=@bairro, localidade=@localidade, uf=@uf, ibge=@ibge, gia=@gia, ddd=@ddd, siafi=@siafi " +
+                                                       "WHERE cep=@cep", connection))
+                {
+                    command.Parameters.AddWithValue("@logradouro", endereco?.Logradouro);
+                    command.Parameters.AddWithValue("@complemento", endereco?.Complemento);
+                    command.Parameters.AddWithValue("@bairro", endereco?.Bairro);
+                    command.Parameters.AddWithValue("@localidade", endereco?.Localidade);
+                    command.Parameters.AddWithValue("@uf", endereco?.UF);
+                    command.Parameters.AddWithValue("@ibge", endereco?.IBGE);
+                    command.Parameters.AddWithValue("@gia", endereco?.GIA);
+                    command.Parameters.AddWithValue("@ddd", endereco?.DDD);
+                    command.Parameters.AddWithValue("@siafi", endereco?.SIAFI);
+                    command.Parameters.AddWithValue("@cep", endereco?.CEP);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+
+        private bool VerificarRegistroExistenteNoBancoDeDados(string cep)
+        {
+            using (var connection = new NpgsqlConnection("Host=localhost:5432;Username=postgres;Password=1478951;Database=postgres"))
+            {
+                connection.Open();
+
+                using (var command = new NpgsqlCommand("SELECT COUNT(*) FROM endereco WHERE cep=@cep", connection))
+                {
+                    command.Parameters.AddWithValue("@cep", cep);
+
+                    int count = Convert.ToInt32(command.ExecuteScalar());
+
+                    return count > 0;
+                }
+            }
+        }
+        private void RemoverEnderecoDoBancoDeDados(string cep)
+        {
+            {
+                using (var connection = new NpgsqlConnection("Host=localhost:5432;Username=postgres;Password=1478951;Database=postgres"))
+                {
+                    connection.Open();
+
+                    using (var command = new NpgsqlCommand("DELETE FROM endereco WHERE cep=@cep", connection))
+                    {
+                        command.Parameters.AddWithValue("@cep", cep);
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+    private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
+   public class Endereco
+    {
+        public string CEP { get; set; }
+        public string Logradouro { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Localidade { get; set; }
+        public string UF { get; set; }
+        public string IBGE { get; set; }
+        public string GIA { get; set; }
+        public string DDD { get; set; }
+        public string SIAFI { get; set; }
+    }
